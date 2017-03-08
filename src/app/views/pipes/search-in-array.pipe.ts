acfdcf3c0dafa85a2110ff5c7fdfe92db4c932b3
SAPIENT\smis64 <shubhankarmisra.bit@gmail.com>
2017-03-08 09:20:00 +0530

    adding the pipe for filtering the array on the basis of a string

--- src/app/views/pipes/search-in-array.pipe.ts
+++ src/app/views/pipes/search-in-array.pipe.ts
@@ -8,7 +8,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Search implements PipeTransform {
 
   transform(value:string,arr:Array<string>):Array<string>  {
+    
+    var reg=new RegExp(value),
+     filteredArray=[];
+  
+     filteredArray=arr.filter(function(element){
+       if(reg.test(element)){
+         return true;
+       }
+       else{
+         return false;
+       }
+     })
 
-   return arr;
+   return filteredArray;
   }
 }
